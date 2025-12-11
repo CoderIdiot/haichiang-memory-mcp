ade6622decf80aa090a1c43fc55e230882924eea
haichiang <rq8686@qq.com>
2025-12-11 23:50:20 +0800

    feat(translate_agent): add create_translate_agent function
    Implement function to create translation agent using deepseek model from settings

--- src/agents/translate_agent/translate_agent.py
+++ src/agents/translate_agent/translate_agent.py
@@ -1,6 +1,17 @@
+from langchain.agents import create_agent
+
 from conf import GlobalLogger
 
-from src.inf.llm.deepseek import G_DeepSeekLLMClient
+from src.inf.env.env_conf import G_Settings
+
+def create_translate_agent():
+    translate_agent = create_agent(
+        model=G_Settings.deepseek_api_chat_model,
+    )
+
+    return translate_agent
+    
+
 
 if __name__ == "__main__":
     try:
